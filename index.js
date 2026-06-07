0c07fe69078d788eb0f32dff1c79de88d86d060f
chaeheun <alzl7410@gmail.com>
2026-06-07 17:18:02 +0900

    fix: index.js

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ let text = `# Hi there 👋
 
 ## 이런 환경에 익숙해요✍🏼
 
-## 언어
-
+### Backend
 <p>
-  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/> 
-  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
+  <img alt="Java" src="https://img.shields.io/badge/Java-007396?style=flat-square&logo=OpenJDK&logoColor=white"/>
+  <img alt="Spring Boot" src="https://img.shields.io/badge/Spring_Boot-6DB33F?style=flat-square&logo=Spring-Boot&logoColor=white"/>
+</p>
+
+### Database
+<p>
+  <img alt="MySQL" src="https://img.shields.io/badge/MySQL-4479A1?style=flat-square&logo=MySQL&logoColor=white"/>
+  <img alt="PostgreSQL" src="https://img.shields.io/badge/PostgreSQL-4169E1?style=flat-square&logo=PostgreSQL&logoColor=white"/>
 </p>
 
 ## 📕 Latest Blog Posts
@@ -28,25 +33,30 @@ const parser = new Parser({
 
 (async () => {
 
-    // 피드 목록
-    const feed = await parser.parseURL('https://devpad.tistory.com/rss'); // 본인의 블로그 주소
-    
-    text += `<ul>`;
-    
-    // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
-    for (let i = 0; i < 10; i++) {
-        const {title, link} = feed.items[i];
-        console.log(`${i + 1}번째 게시물`);
-        console.log(`추가될 제목: ${title}`);
-        console.log(`추가될 링크: ${link}`);
-        text += `<li><a href='${link}' target='_blank'>${title}</a></li>`;
-    }
+    try {
+        // 피드 목록 (요청하신 티스토리 주소의 RSS)
+        const feed = await parser.parseURL('https://chee0630.tistory.com/rss');
 
-    text += `</ul>`;
-    
-    // README.md 파일 생성
-    writeFileSync('README.md', text, 'utf8', (e) => {
-        console.log(e);
-    })
-    console.log('업데이트 완료');
+        text += `<ul>`;
+
+        // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
+        for (let i = 0; i < 10; i++) {
+            // 혹시 작성된 글이 10개 미만일 경우를 대비한 예외 처리
+            if (!feed.items[i]) break;
+
+            const {title, link} = feed.items[i];
+            console.log(`${i + 1}번째 게시물`);
+            console.log(`추가될 제목: ${title}`);
+            console.log(`추가될 링크: ${link}`);
+            text += `<li><a href='${link}' target='_blank'>${title}</a></li>`;
+        }
+
+        text += `</ul>`;
+
+        // README.md 파일 생성
+        writeFileSync('README.md', text, 'utf8');
+        console.log('업데이트 완료');
+    } catch (error) {
+        console.error('RSS 피드를 가져오는 중 에러가 발생했습니다:', error);
+    }
 })();
